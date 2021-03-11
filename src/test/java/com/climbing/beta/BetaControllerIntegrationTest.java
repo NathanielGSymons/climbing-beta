79f9ea8e79d4257330ebb24f7dd2139355a76ac2
NathanielGSymons <n.gsymons@gmail.com>
2021-03-11 12:04:03 +0000

    added preliminary integration test

---
+++ src/test/java/com/climbing/beta/BetaControllerIntegrationTest.java
@@ -0,0 +1,50 @@
+package com.climbing.beta;
+
+import com.climbing.beta.db.entity.ClimbBeta;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(classes = Application.class)
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+public class BetaControllerIntegrationTest {
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        objectMapper = new ObjectMapper();
+    }
+
+    @Test
+    void serverReturnsBetaUrl() throws Exception {
+        ClimbBeta climbBeta = new ClimbBeta();
+        climbBeta.setId(1);
+        climbBeta.setCrag("crag");
+        climbBeta.setClimb("climb");
+        climbBeta.setBetaUrl("betaUrl");
+
+        String climbBetaAsJson = objectMapper.writeValueAsString(climbBeta);
+
+        MockHttpServletRequestBuilder requestBuilder = get("/api/crag/crag/climb/climb")
+                .contentType(MediaType.APPLICATION_JSON);
+
+        this.mockMvc.perform(requestBuilder)
+                .andExpect(status().isOk())
+                .andExpect(content().string(climbBetaAsJson));
+    }
+}
